6c1757437370c513f1ae056d852438ff94276103
liampost21 <35525269+liampost21@users.noreply.github.com>
2018-02-14 13:07:24 +0000

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Agile_Project.Startup))]
+namespace Agile_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
