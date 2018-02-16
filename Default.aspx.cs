c3d5c7f15155aec7a2a5d36984363f732ac9ecca
Liam Post <liampost21@hotmail.co.uk>
2018-02-16 14:16:33 +0000

    commit

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,25 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Configuration;
-using System.Data.SqlClient;
 
 public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string connString = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString;
-
-        SqlConnection conn = new SqlConnection(connString);
-        try
-        {
-            conn.Open();
-        }
-        catch (Exception f)
-        {
-            Console.WriteLine(f.ToString());
-        }
-        conn.Close();
 
     }
 }
